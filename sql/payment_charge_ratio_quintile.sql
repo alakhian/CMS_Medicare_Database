0e622a4f396461e30913bba6cd38a8499922dc11
Amar Lakhian <amar.lakhian@utexas.edu>
2015-05-15 02:55:24 -0500

    physician table reproducibility info

--- sql/payment_charge_ratio_quintile.sql
+++
@@ -1,5 +0,0 @@
-create table inpatient_2012_data as
-(select i.*, (CASE  WHEN i.PAYMENT_CHARGEDAMT_RATIO < (0.25) THEN 'Less than 25%'
-   WHEN i.PAYMENT_CHARGEDAMT_RATIO > (0.5)  THEN 'Greater than 50%'
-   ELSE 'Between 25% and 50%' END ) payment_charge_ratio_level from inpatient_2012_data_avg i);
-
